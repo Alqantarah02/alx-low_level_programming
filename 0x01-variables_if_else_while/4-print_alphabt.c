873c1f1aba15323611d46f16e0bf3c5caab1979f
alqantarah02 <alqantarah02@gmail.com>
2022-04-10 11:31:21 -0700

    variables_if_else

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
  */
 int main(void)
 {
-char low, e, q;
-
-e = 'e';
-q = 'q';
-
-for (low = 'a'; low <= 'z'; low++)
-
-if (low != e && low != q)
-putchar(low);
+char c = 'a';
+while (c <= 'z')
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
 }
-printf("\n");
-
+c++;
+}
+putchar('\n');
 return (0);
 }
