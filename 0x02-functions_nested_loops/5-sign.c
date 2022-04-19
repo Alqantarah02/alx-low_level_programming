9554b9296c3e778ea9aed781d8e718046b2b5559
alqantarah02 <alqantarah02@gmail.com>
2022-04-18 17:01:06 -0700

    fn

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_sign - check numbers
- * @n: number to check 
- * Return: 0 or 1
- */
+*print_sign - check numbers
+*@n: number to check
+*Return: 0 or 1
+*/
 int print_sign(int n)
 {
 int test;
@@ -20,7 +20,7 @@ _putchar('0');
 else
 {
 test = -1;
-_putchar('_');
+_putchar('-');
 }
 return (test);
 }
